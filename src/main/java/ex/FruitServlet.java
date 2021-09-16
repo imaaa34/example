ef6f0d2d4b1b63dc96f0c5d0ad8c319ac2bf4935
imaaa34 <oct.imd34@gmail.com>
2021-09-16 14:38:39 +0900

    [Finish] chapter8

--- src/main/java/ex/FruitServlet.java
+++ src/main/java/ex/FruitServlet.java
@@ -8,12 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/FruitServlet")
 public class FruitServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Fruit fruit = new Fruit("いちご", 700);
-		request.setAttribute("fruit", fruit);
+		Fruit fruit = new Fruit("いちご", 800);
+//		request.setAttribute("fruit", fruit);
+		HttpSession session = request.getSession();
+		session.setAttribute("fruit", fruit);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/ex/fruit.jsp");
 		dispatcher.forward(request, response);
 	}
