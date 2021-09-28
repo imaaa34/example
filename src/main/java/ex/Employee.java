3af50ef6259936df369aef957ca4ed25ada629b7
imaaa34 <oct.imd34@gmail.com>
2021-09-29 00:21:12 +0900

    [Add] Login test method

--- src/main/java/ex/Employee.java
+++ src/main/java/ex/Employee.java
@@ -2,9 +2,14 @@ package ex;
 public class Employee {
 	private String id;
 	private String name;
+	private int age;
 	public Employee(String id, String name) {
 		this.id = id; this.name = name;
 	}
+	public Employee(String id, String name, int age) {
+		this.id = id; this.name = name; this.age = age;
+	}
 	public String getId() { return id; }
 	public String getName() { return name; }
+	public int getAge() { return age; }
 }
